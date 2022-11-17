b2358efaac83a295260ddbcde536ad050baeecfa
Ahmed-Rokenddine <masteriko05@gmail.com>
2022-11-17 10:11:34 +0100

    small changes

--- js/script.js
+++ js/script.js
@@ -16,3 +16,21 @@ window.onscroll = () => {
 	menu.classList.remove('bx-x');
 	navbar.classList.remove('open');
 };
+window.addEventListener("scroll", function() {showFunction();showFunction2()});
+
+function showFunction() {
+	if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+		document.getElementById("toptexts2").style.display = "block";
+	} else {
+		document.getElementById("toptexts2").style.display = "none";
+	}
+}
+function showFunction2() {
+	if (document.body.scrollTop > 3300 || document.documentElement.scrollTop > 3300) {
+		document.getElementById("cover").style.display = "none";
+	} else {
+		document.getElementById("cover").style.display = "block";
+	}
+}
+
+
