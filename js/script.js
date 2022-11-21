a1591318006193abb0d9bc13e2d667c1dcd74821
ELMEHDIBAHIJ <bahijstudies@gmail.com>
2022-11-21 20:05:21 +0100

    mehdi edits

--- js/script.js
+++ js/script.js
@@ -16,13 +16,27 @@ window.onscroll = () => {
 	menu.classList.remove('bx-x');
 	navbar.classList.remove('open');
 };
-window.addEventListener("scroll", function() {showFunction();showFunction2();showFunction3();showFunction4()});
+window.addEventListener("scroll", function() {showFunction();showFunction2();showFunction3();showFunction4();showFunction5();showFunction6()});
 
 function showFunction() {
-	if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+	if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
 		document.getElementById("toptexts2").style.display = "block";
 	} else {
 		document.getElementById("toptexts2").style.display = "none";
+	}
+}
+function showFunction5() {
+	if (document.body.scrollTop > 650 || document.documentElement.scrollTop > 650) {
+		document.getElementById("toptexts3").style.display = "block";
+	} else {
+		document.getElementById("toptexts3").style.display = "none";
+	}
+}
+function showFunction6() {
+	if (document.body.scrollTop > 850 || document.documentElement.scrollTop > 850) {
+		document.getElementById("toptexts4").style.display = "block";
+	} else {
+		document.getElementById("toptexts4").style.display = "none";
 	}
 }
 function showFunction8() {
@@ -53,6 +67,26 @@ function showFunction4() {
 		document.getElementById("single2").style.display = "none";
 	}
 }
+
+// about toggler
+let abt1 = document.querySelector('.abt1');
+let more1 = document.querySelector('.more1');
+let abt2 = document.querySelector('.abt2');
+let more2 = document.querySelector('.more2');
+let abt3 = document.querySelector('.abt3');
+let more3 = document.querySelector('.more3');
+
+
+abt1.addEventListener('click', function() {
+    more1.classList.toggle('show-more')
+})
+abt2.addEventListener('click', function() {
+    more2.classList.toggle('show-more')
+})
+abt3.addEventListener('click', function() {
+    more3.classList.toggle('show-more')
+})
+
 
 
 
