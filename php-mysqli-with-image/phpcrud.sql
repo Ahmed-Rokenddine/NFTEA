a79eacd9c7ac06addde6c5f89028bf6cc2a638f2
Ahmed-Rokenddine <masteriko05@gmail.com>
2022-12-02 14:50:35 +0100

    Uploaded sql file

--- php-mysqli-with-image/phpcrud.sql
+++ php-mysqli-with-image/phpcrud.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 20, 2020 at 11:03 AM
--- Server version: 10.4.8-MariaDB
--- PHP Version: 7.3.11
+-- Generation Time: Dec 02, 2022 at 02:49 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,6 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `collection`
+--
+
+CREATE TABLE `collection` (
+  `ID` int(10) NOT NULL,
+  `collname` varchar(200) DEFAULT NULL,
+  `artiste` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_croatian_ci DEFAULT NULL,
+  `CreationDate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `collection`
+--
+
+INSERT INTO `collection` (`ID`, `collname`, `artiste`, `CreationDate`) VALUES
+(4, 'jojo', 'diligi', '2022-11-23 13:11:39'),
+(7, 'Digital', 'Zeus', '2022-11-29 13:26:24');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tblusers`
 --
 
@@ -32,8 +52,9 @@ CREATE TABLE `tblusers` (
   `ID` int(10) NOT NULL,
   `nftname` varchar(200) DEFAULT NULL,
   `Prix` bigint(10) DEFAULT NULL,
-  `Collection` varchar(200) DEFAULT NULL,
+  `Collection` int(10) NOT NULL,
   `Description` mediumtext DEFAULT NULL,
+  `nftpic` varchar(200) DEFAULT NULL,
   `CreationDate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -41,27 +62,52 @@ CREATE TABLE `tblusers` (
 -- Dumping data for table `tblusers`
 --
 
-
+INSERT INTO `tblusers` (`ID`, `nftname`, `Prix`, `Collection`, `Description`, `nftpic`, `CreationDate`) VALUES
+(35, 'Killer cat', 24000, 4, 'a good kitty ', 'd41d8cd98f00b204e9800998ecf8427e1669728406.jpg', '2022-11-30 13:26:50'),
+(36, 'Simple', 11000, 4, 'a good picture', 'd41d8cd98f00b204e9800998ecf8427e1669728443.jpg', '2022-11-29 13:27:23');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `collection`
+--
+ALTER TABLE `collection`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- Indexes for table `tblusers`
 --
 ALTER TABLE `tblusers`
-  ADD PRIMARY KEY (`ID`);
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `Collection` (`Collection`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `collection`
+--
+ALTER TABLE `collection`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT for table `tblusers`
 --
 ALTER TABLE `tblusers`
-  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tblusers`
+--
+ALTER TABLE `tblusers`
+  ADD CONSTRAINT `Collection` FOREIGN KEY (`Collection`) REFERENCES `collection` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
